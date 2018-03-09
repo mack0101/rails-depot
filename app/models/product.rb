07cc922598b32624d825380a57daa6c9c0782135
Maciek Kwiatkowski <kwiatkowskimack@gmail.com>
2018-03-09 22:34:37 +1100

    testing something

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif\jpg\png)\Z},
     message: 'must be a URL for GIF, JPG or PNG image.'
-  }  
+  }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
+# testing something
